99535796adad3fb5f9a9137dc051daf5691323ee
farhanmiftakhul <farhanmiftakhulfarruq@gmail.com>
2026-04-27 15:46:30 +0700

    chore: update default application and environment URLs for production deployment

--- config/app.php
+++ config/app.php
@@ -52,8 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
-    'frontend_url' => env('FRONTEND_URL', 'http://localhost:5173'),
+    'url' => env('APP_URL', 'https://optik-medio.laravel.cloud'),
+    'frontend_url' => env('FRONTEND_URL', 'https://optik-medio-shop.vercel.app'),
 
     /*
     |--------------------------------------------------------------------------
