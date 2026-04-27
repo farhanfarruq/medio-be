7315382344778ecafbba8d597c15c0e8d6ef7480
farhanmiftakhul <farhanmiftakhulfarruq@gmail.com>
2026-04-27 14:32:16 +0700

    refactor: split consolidated Filament resource pages into individual files for better maintainability

--- app/Filament/Resources/AppSettingResource/Pages/EditAppSetting.php
+++ app/Filament/Resources/AppSettingResource/Pages/EditAppSetting.php
@@ -3,14 +3,7 @@
 namespace App\Filament\Resources\AppSettingResource\Pages;
 
 use App\Filament\Resources\AppSettingResource;
-use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Pages\EditRecord;
-
-class ListAppSettings extends ListRecords
-{
-    protected static string $resource = AppSettingResource::class;
-}
 
 class EditAppSetting extends EditRecord
 {
--- app/Filament/Resources/PaymentResource/Pages/EditPayment.php
+++ app/Filament/Resources/PaymentResource/Pages/EditPayment.php
@@ -3,14 +3,7 @@
 namespace App\Filament\Resources\PaymentResource\Pages;
 
 use App\Filament\Resources\PaymentResource;
-use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Pages\EditRecord;
-
-class ListPayments extends ListRecords
-{
-    protected static string $resource = PaymentResource::class;
-}
 
 class EditPayment extends EditRecord
 {
